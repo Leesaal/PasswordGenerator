94f26e6b77688606672021b73a4eccb81fc55d54
Leesa <118930290+Leesaal@users.noreply.github.com>
2023-02-16 21:06:30 +0000

    Improved commenting

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ var includeUpper = document.getElementById("includeUpper");
 var includeLower = document.getElementById("includeLower");
 var includeNumbers = document.getElementById("includeNumbers");
 var includeSpecial = document.getElementById("includeSpecial");
+
+// Create arrays of characters to choose random characters from
 var upperChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numberChars = ["0", "1", "2", "3", "4", "5","6", "7", "8", "9"];
